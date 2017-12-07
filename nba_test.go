c3a0c335f3fc3ef77b30c8011f80645a95f024de
Mike Wu <mwu@vsco.co>
2017-12-07 11:30:48 -0800

    add test for ActivePlayers endpoint

--- nba_test.go
+++ nba_test.go
@@ -16,6 +16,35 @@ var data = map[string]interface{}{
 	"TeamId":   int64(86),
 }
 
+func TestActivePlayers(t *testing.T) {
+	players := make([]*models.NBAPlayer, 0)
+	players = append(players, fixtures.MakeNBAPlayer(data))
+	rawData := map[string]interface{}{
+		"activeplayers": map[string]interface{}{
+			"playerentry": players,
+		},
+	}
+	httpClient, mux, server := testServer()
+	defer server.Close()
+	mux.HandleFunc(buildPath("/v1.1/pull/nba/2017-2018-regular", activePlayersPath), func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "GET", r.Method)
+		w.Header().Set("Content-Type", "application/json")
+		jsonStr, err := json.Marshal(rawData)
+		if err != nil {
+			fmt.Fprintf(w, "Error")
+		} else {
+			fmt.Fprintf(w, string(jsonStr))
+		}
+	})
+	client := NewClient(httpClient, "test", "test")
+	opts := ActivePlayersOptions{
+		Limit: 1,
+	}
+	data, err := client.NBA.ActivePlayers("2017-2018-regular", opts)
+	assert.Equal(t, players, data)
+	assert.Nil(t, err)
+}
+
 func TestCumulativePlayerStats(t *testing.T) {
 	players := make([]*models.NBACumulativePlayerStats, 0)
 	players = append(players, fixtures.MakeNBACumulativePlayerStats(data))
@@ -30,7 +59,6 @@ func TestCumulativePlayerStats(t *testing.T) {
 		assert.Equal(t, "GET", r.Method)
 		w.Header().Set("Content-Type", "application/json")
 		jsonStr, err := json.Marshal(cumulativeRawData)
-		fmt.Printf("%s %+v\n", string(jsonStr), err)
 		if err != nil {
 			fmt.Fprintf(w, "Error")
 		} else {
