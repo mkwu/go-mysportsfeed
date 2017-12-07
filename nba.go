dd0768426b209d1de18e316f9c06af40d8d43fab
Mike Wu <mwu@vsco.co>
2017-12-07 11:20:35 -0800

    add support for activeplayers

--- nba.go
+++ nba.go
@@ -11,9 +11,16 @@ import (
 
 const (
 	cumulativePlayerStatsPath = "cumulative_player_stats"
+	activePlayersPath         = "active_players"
 )
 
 type CumulativePlayerStatsOptions struct {
+	Limit    int    `url:"limit"`
+	Team     string `url:"team"`
+	position string `url:"position"`
+}
+
+type ActivePlayersOptions struct {
 	Limit    int    `url:"limit"`
 	Team     string `url:"team"`
 	position string `url:"position"`
@@ -57,6 +64,38 @@ func (s *NBAService) CumulativePlayerStats(season string, opts CumulativePlayerS
 		}
 	} else {
 		err = errors.New("Expecting cumulativeplayerstats in response")
+	}
+	return
+}
+
+func (s *NBAService) ActivePlayers(season string, opts ActivePlayersOptions) (players []*models.NBAPlayer, err error) {
+	var results map[string]interface{}
+	players = make([]*models.NBAPlayer, 0)
+	_, err = s.sling.New().Get(buildPath(season, activePlayersPath)).QueryStruct(opts).Receive(&results, err)
+	if err != nil {
+		return
+	}
+	if data, ok := results["activeplayers"]; ok {
+		data := data.(map[string]interface{})
+		if _, ok := data["playerentry"]; ok {
+			switch reflect.TypeOf(data["playerentry"]).Kind() {
+			case reflect.Slice:
+				list := reflect.ValueOf(data["playerentry"])
+				for i := 0; i < list.Len(); i++ {
+					player := new(models.NBAPlayer)
+					player.IsActive = true
+					p := list.Index(i).Interface().(map[string]interface{})
+					err = Unmarshal(p, player)
+					players = append(players, player)
+				}
+			default:
+				err = fmt.Errorf("Expecting array, got %s", reflect.TypeOf(data["playerentry"]).Kind())
+			}
+		} else {
+			err = errors.New("Expecting playerentry in response")
+		}
+	} else {
+		err = errors.New("Expecting activeplayers in response")
 	}
 	return
 }
