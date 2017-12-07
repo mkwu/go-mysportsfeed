c38150eec3fdbf4fc38a16f438f4449533be14a1
Mike Wu <mwu@vsco.co>
2017-12-07 11:30:38 -0800

    add nbaplayer fixture for unit tests

--- util/fixtures/nba.go
+++ util/fixtures/nba.go
@@ -5,6 +5,15 @@ import (
 	"github.com/mkwu/go-mysportsfeed/models"
 )
 
+var NBAPlayerFactory = factory.NewFactory(
+	&models.NBAPlayer{},
+)
+
+func MakeNBAPlayer(data map[string]interface{}) *models.NBAPlayer {
+	data["IsActive"] = true
+	return NBAPlayerFactory.MustCreateWithOption(data).(*models.NBAPlayer)
+}
+
 var NBACumulativePlayerStatsFactory = factory.NewFactory(
 	&models.NBACumulativePlayerStats{},
 )
